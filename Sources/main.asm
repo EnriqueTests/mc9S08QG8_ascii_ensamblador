1c9cda08536c59f3f50eb60d2a8a70f158f653fa
EnriqueTests <82471440+EnriqueTests@users.noreply.github.com>
2021-10-17 01:49:14 -0500

    terminado

---
+++ Sources/main.asm
@@ -0,0 +1,86 @@
+;*******************************************************************
+;* This stationery serves as the framework for a user application. *
+;* For a more comprehensive program that demonstrates the more     *
+;* advanced functionality of this processor, please see the        *
+;* demonstration applications, located in the examples             *
+;* subdirectory of the "Freescale CodeWarrior for HC08" program    *
+;* directory.                                                      *
+;*******************************************************************
+
+; Include derivative-specific definitions
+            INCLUDE 'derivative.inc'
+            
+;
+; export symbols
+;
+            XDEF _Startup
+            ABSENTRY _Startup
+
+;
+; variable/data section
+;
+            ORG    $60         	; Insert your data definition here
+M60: DS.B   1					; Dato
+M61: DS.B   1					; MSB
+M62: DS.B   1					; LSB
+
+;
+; code section
+;
+            ORG    ROMStart
+            
+
+_Startup:
+			LDA	   	#$12			; Inmediato 	A=$12 hexa Quitar el WATCHDOG
+			STA		SOPT1			; Directo 	Guardar A en SOPT1
+			
+			LDX		#$60
+			MOV		#$5A, M60
+reinicio:
+			LDA		M60				; Cargar dato en acumulador
+			CBEQX 	#$61,intercambiar_nibbles_acumulador
+			CBEQX 	#$62,mainLoop	; Fin
+continuar:
+			AND		#$F0			; Nibble correspondiente de M60 en acumulador
+			NSA
+			STA		1,X				; Guardar temporalmente en memoria
+			
+			SUB		#$09			; Restar 9, si da cero o negativo es un dato igual a nueve o menor
+			BLE 	menor_igual_a_9	; menor o igual a 9
+			BHI 	mayor_a_9		; mayor a 9
+intercambiar_nibbles_acumulador:
+			NSA						; intercambio
+			BRA		continuar
+menor_igual_a_9:
+			LDA		1,X				; Cargar dato de memoria en acumulador
+			ADD		#$30			; Sumar #$30
+			STA		1,X				; Guardar en memoria
+			INCX
+			BRA		reinicio
+mayor_a_9:
+			LDA		1,X				; Cargar dato de memoria en acumulador
+			ADD		#$37			; Sumar #$37
+			STA		1,X				; Guardar en memoria
+			INCX
+			BRA		reinicio
+mainLoop:
+            BRA    mainLoop
+			
+;**************************************************************
+;* spurious - Spurious Interrupt Service Routine.             *
+;*             (unwanted interrupt)                           *
+;**************************************************************
+
+spurious:				; placed here so that security value
+			NOP			; does not change all the time.
+			RTI
+
+;**************************************************************
+;*                 Interrupt Vectors                          *
+;**************************************************************
+
+            ORG	$FFFA
+
+			DC.W  spurious			;
+			DC.W  spurious			; SWI
+			DC.W  _Startup			; Reset
